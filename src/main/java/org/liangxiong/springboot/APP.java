85c969df74e85a243df4fee3ce48b657d065629b
liangxiong <1071608617@qq.com>
2018-10-03 19:00:25 +0800

    <chore><all>:<init project>

---
+++ src/main/java/org/liangxiong/springboot/APP.java
@@ -0,0 +1,18 @@
+package org.liangxiong.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liangxiong
+ * Date:2018-10-03
+ * Time:18:57
+ * @Description 项目启动类
+ */
+@SpringBootApplication
+public class APP {
+
+    public static void main(String[] args) {
+        SpringApplication.run(APP.class, args);
+    }
+}

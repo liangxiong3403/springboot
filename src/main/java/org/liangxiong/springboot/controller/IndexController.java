8793c10aef976cc105a3c1fd2174c86c62a9351e
liangxiong <1071608617@qq.com>
2018-11-07 15:38:56 +0800

    <fix><test>:<complete springboot test>

--- src/main/java/org/liangxiong/springboot/controller/IndexController.java
+++ src/main/java/org/liangxiong/springboot/controller/IndexController.java
@@ -1,12 +1,15 @@
 package org.liangxiong.springboot.controller;
 
+import org.liangxiong.springboot.entity.School;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author liangxiong
@@ -19,6 +22,9 @@ public class IndexController {
 
     @Autowired
     private ApplicationContext context;
+
+    @Autowired
+    private RestTemplateBuilder builder;
 
     /**
      * JSP视图测试
@@ -86,5 +92,18 @@ public class IndexController {
     @GetMapping("/exit")
     public void exit() {
         SpringApplication.exit(context);
+    }
+
+    /**
+     * 通过RestTemplate发送请求
+     *
+     * @return
+     */
+    @GetMapping("/template")
+    @ResponseBody
+    public School testClient() {
+        // 通过SpringBoot提供地RestTemplateBuilder进行RestTemplate的自定义
+        RestTemplate restTemplate = builder.basicAuthorization("admin", "123456").build();
+        return restTemplate.getForObject("http://localhost:9999/rest/json/school", School.class);
     }
 }

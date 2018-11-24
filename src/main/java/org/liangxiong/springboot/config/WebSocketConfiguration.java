d7201d06cfe9b7dac08b8679cb2c5adf4054538b
liangxiong <1071608617@qq.com>
2018-11-24 19:22:54 +0800

    <feat><websocket>:<add websocket api>

---
+++ src/main/java/org/liangxiong/springboot/config/WebSocketConfiguration.java
@@ -0,0 +1,39 @@
+package org.liangxiong.springboot.config;
+
+import org.liangxiong.springboot.websocket.MessageServerEndpoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author liangxiong
+ * @Date:2018-11-24
+ * @Time:15:30
+ * @Description WebSocket配置类
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration {
+
+    /**
+     * If you want to use @ServerEndpoint in a Spring Boot application that used an embedded container,
+     * you must declare a single ServerEndpointExporter @Bean
+     *
+     * @return
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+    /**
+     * 自定义实现地消息端点
+     *
+     * @return
+     */
+    @Bean
+    public MessageServerEndpoint messageServerEndpoint() {
+        return new MessageServerEndpoint();
+    }
+}
